30b56a0e7809cc9479121cd4eec2509d3b5ab353
Irina <irinak1804@gmail.com>
2021-02-16 16:47:15 +0300

    добавление аннотаций testng

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -1,33 +1,50 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
 
 // тест на добавления нового юзера
 public class Test1 {
 
     private static WebDriver driver;
+    private User user;
 
-    public static void main(String[] args) {
-
+    //Самый первый
+    @BeforeClass
+    private void setUp() {
         // путь к файлу драйвера
         System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
         //новый объект для запуска браузера
         driver = new ChromeDriver();
 
+    }
+
+    //Код из аннотации @BeforeMethod выполняется перед выполнением методов, отмеченных аннотаций @Test.
+    @BeforeMethod
+    private void testFixtures() {
         // метод гет для перехода на страницу
         driver.get("http://localhost:8080/users");
-
-        // вызываем метод заполнения формы
-        fillUser(new User("Иван", "Петров", "Москва"));
-
-        // вызываем метода добавления юзера
-        addUser();
-
-        // закрываем браузер
-        driver.quit();
-
+        //создание юзера
+        user = new User("Иван", "Петров", "Москва");
 
     }
+
+    // после выполнения всех тестовых методов
+    @AfterClass
+    public void cleanup() {
+        // закрываем браузер
+        driver.quit();
+    }
+
+
+    @Test
+    public void createUserTest() {
+        // вызываем метод заполнения формы
+        fillUser(user);
+        // вызываем метода добавления юзера
+        addUser();
+    }
+
 
     // метод для заполнения формы
     private static void fillUser(User user) {
